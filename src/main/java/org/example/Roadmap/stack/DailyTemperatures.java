8174c29ddbc9fd05a0a4a6859239214547f492d0
HungCao <caohung304@gmail.com>
2024-07-17 11:05:11 +0700

    17-7-2024

---
+++ src/main/java/org/example/Roadmap/stack/DailyTemperatures.java
@@ -0,0 +1,32 @@
+package org.example.Roadmap.stack;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Stack;
+//passed. 70%
+public class DailyTemperatures {
+    //Given an array of integers temperatures represents the daily temperatures, return an array answer such that
+    // answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day
+    // for which this is possible, keep answer[i] == 0 instead.
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println(dailyTemperatures(temperatures));
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int []result = new int[length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index =  stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
