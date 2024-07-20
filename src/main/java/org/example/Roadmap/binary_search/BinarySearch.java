cc87dba4f26b81c345b39cbeb0417b762ed402f5
quanghung304 <caohung304@gmail.com>
2024-07-20 16:01:36 +0700

    20-7-2024

---
+++ src/main/java/org/example/Roadmap/binary_search/BinarySearch.java
@@ -0,0 +1,39 @@
+package org.example.Roadmap.binary_search;
+
+public class BinarySearch {
+    //Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to
+    // search target in nums. If target exists, then return its index. Otherwise, return -1.
+    //
+    //You must write an algorithm with O(log n) runtime complexity.
+    public static void main(String[] args) {
+        int[] nums = {1,2};
+        int target = -3;
+//        int[] nums = {2,5};
+//        int target = 5;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = left + (right - left) / 2;
+
+        while (left <= right) {
+            if (left == right && nums[middle] != target) {
+                return -1;
+            }
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+
+            middle = left + (right - left) / 2;
+        }
+
+        return -1;
+    }
+}
