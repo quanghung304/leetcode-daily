d7446395c5ad5a5fe209ba025a635a0acc367741
HungCao <caohung304@gmail.com>
2024-08-30 17:08:35 +0700

    30-8-2024

---
+++ src/main/java/org/example/Roadmap/trees/BalancedBinaryTree.java
@@ -0,0 +1,30 @@
+package org.example.Roadmap.trees;
+
+import org.example.DataStructure.TreeNode;
+
+public class BalancedBinaryTree {
+    //Given a binary tree, determine if it is height-balanced
+    public static void main(String[] args) {
+        Integer[] nums = {1,2,2,3,3,null,null,4,4};
+        TreeNode root = TreeNode.buildTree(nums);
+        System.out.println(isBalanced(root));
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        if (traverse(root) > 1000) return false;
+        return true;
+    }
+
+    private static int traverse(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+
+        if (Math.abs(left-right) > 1) return 1000;
+
+        return Math.max(left, right)+1;
+    }
+}
